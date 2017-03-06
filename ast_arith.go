c8e0b9df6bf33dad122c5e4510dad4de61d923b0
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-03-06 12:21:35 -0800

    Add more arithmetic operations

--- ast_arith.go
+++ ast_arith.go
@@ -26,6 +26,44 @@ func (a *AST) Add(args ...*AST) *AST {
 	}
 }
 
+// Mul creates an AST node representing multiplication.
+//
+// All AST values must be part of the same context.
+func (a *AST) Mul(args ...*AST) *AST {
+	raws := make([]C.Z3_ast, len(args)+1)
+	raws[0] = a.rawAST
+	for i, arg := range args {
+		raws[i+1] = arg.rawAST
+	}
+
+	return &AST{
+		rawCtx: a.rawCtx,
+		rawAST: C.Z3_mk_mul(
+			a.rawCtx,
+			C.uint(len(raws)),
+			(*C.Z3_ast)(unsafe.Pointer(&raws[0]))),
+	}
+}
+
+// Sub creates an AST node representing subtraction.
+//
+// All AST values must be part of the same context.
+func (a *AST) Sub(args ...*AST) *AST {
+	raws := make([]C.Z3_ast, len(args)+1)
+	raws[0] = a.rawAST
+	for i, arg := range args {
+		raws[i+1] = arg.rawAST
+	}
+
+	return &AST{
+		rawCtx: a.rawCtx,
+		rawAST: C.Z3_mk_sub(
+			a.rawCtx,
+			C.uint(len(raws)),
+			(*C.Z3_ast)(unsafe.Pointer(&raws[0]))),
+	}
+}
+
 // Lt creates a "less than" comparison.
 //
 // Maps to: Z3_mk_lt
@@ -36,6 +74,16 @@ func (a *AST) Lt(a2 *AST) *AST {
 	}
 }
 
+// Le creates a "less than" comparison.
+//
+// Maps to: Z3_mk_le
+func (a *AST) Le(a2 *AST) *AST {
+	return &AST{
+		rawCtx: a.rawCtx,
+		rawAST: C.Z3_mk_le(a.rawCtx, a.rawAST, a2.rawAST),
+	}
+}
+
 // Gt creates a "greater than" comparison.
 //
 // Maps to: Z3_mk_gt
@@ -43,5 +91,15 @@ func (a *AST) Gt(a2 *AST) *AST {
 	return &AST{
 		rawCtx: a.rawCtx,
 		rawAST: C.Z3_mk_gt(a.rawCtx, a.rawAST, a2.rawAST),
+	}
+}
+
+// Ge creates a "less than" comparison.
+//
+// Maps to: Z3_mk_ge
+func (a *AST) Ge(a2 *AST) *AST {
+	return &AST{
+		rawCtx: a.rawCtx,
+		rawAST: C.Z3_mk_ge(a.rawCtx, a.rawAST, a2.rawAST),
 	}
 }
--- ast_arith_test.go
+++ ast_arith_test.go
@@ -25,6 +25,48 @@ func TestASTAdd(t *testing.T) {
 	}
 }
 
+func TestASTMul(t *testing.T) {
+	config := NewConfig()
+	defer config.Close()
+
+	ctx := NewContext(config)
+	defer ctx.Close()
+
+	// Create an int
+	v1 := ctx.Int(1, ctx.IntSort())
+	v2 := ctx.Int(2, ctx.IntSort())
+	v3 := ctx.Int(3, ctx.IntSort())
+
+	// Mul
+	raw := v1.Mul(v2, v3)
+
+	actual := raw.String()
+	if actual != "(* 1 2 3)" {
+		t.Fatalf("bad:\n%s", actual)
+	}
+}
+
+func TestASTSub(t *testing.T) {
+	config := NewConfig()
+	defer config.Close()
+
+	ctx := NewContext(config)
+	defer ctx.Close()
+
+	// Create an int
+	v1 := ctx.Int(1, ctx.IntSort())
+	v2 := ctx.Int(2, ctx.IntSort())
+	v3 := ctx.Int(3, ctx.IntSort())
+
+	// Sub
+	raw := v1.Sub(v2, v3)
+
+	actual := raw.String()
+	if actual != "(- (- 1 2) 3)" {
+		t.Fatalf("bad:\n%s", actual)
+	}
+}
+
 func TestASTLt(t *testing.T) {
 	config := NewConfig()
 	defer config.Close()
@@ -44,6 +86,25 @@ func TestASTLt(t *testing.T) {
 	}
 }
 
+func TestASTLe(t *testing.T) {
+	config := NewConfig()
+	defer config.Close()
+	ctx := NewContext(config)
+	defer ctx.Close()
+
+	// Create an int
+	x := ctx.Const(ctx.Symbol("x"), ctx.IntSort())
+	y := ctx.Const(ctx.Symbol("y"), ctx.IntSort())
+
+	// Add
+	raw := x.Le(y)
+
+	actual := raw.String()
+	if actual != "(<= x y)" {
+		t.Fatalf("bad:\n%s", actual)
+	}
+}
+
 func TestASTGt(t *testing.T) {
 	config := NewConfig()
 	defer config.Close()
@@ -59,6 +120,25 @@ func TestASTGt(t *testing.T) {
 
 	actual := raw.String()
 	if actual != "(> x y)" {
+		t.Fatalf("bad:\n%s", actual)
+	}
+}
+
+func TestASTGe(t *testing.T) {
+	config := NewConfig()
+	defer config.Close()
+	ctx := NewContext(config)
+	defer ctx.Close()
+
+	// Create an int
+	x := ctx.Const(ctx.Symbol("x"), ctx.IntSort())
+	y := ctx.Const(ctx.Symbol("y"), ctx.IntSort())
+
+	// Add
+	raw := x.Ge(y)
+
+	actual := raw.String()
+	if actual != "(>= x y)" {
 		t.Fatalf("bad:\n%s", actual)
 	}
 }
